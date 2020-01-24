7e857b1f74fb5c876a4e18413b088a4317b4bfc3
Cheol_Kim <cheol.kim@fitogether.com>
2020-01-24 22:38:31 +0900

    Register Component Using TypeScript

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,17 +1,12 @@
 <template>
   <div class="product">
-    <Product inputMessage="props Message from views" />
+    <product-product inputMessage="props Message from views" />
+    <ProductProduct inputMessage="props Message from views" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Product from "@/components/Product/Product.vue";
-
 export default {
-  name: "home",
-  components: {
-    Product
-  }
+  name: "home"
 };
 </script>
